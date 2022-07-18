582c80d5521245fef338be47bda75575350bbacd
Edaly09 <edalyminthe@gmail.com>
2022-07-18 16:27:15 +0200

    emailIsValid

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,32 +13,46 @@ constructor(){
   }
 }
 handleEmailChange = (e) =>{
-	this.setState({ email: e.target.value });
- 
+
+const str = '/^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/';
+
+//   if(this.state.email.test(str)){
+//    this.state.emailIsValid = true
+   
+//  }
+ this.setState({ email: e.target.value });
+ console.log(this.state.email);
+
+  //  console.log("hello")
 
 }
 handlePasswordChange = (e) =>{
 this.setState({ password: e.target.value });
+console.log(this.state.password)
 
 
 }
 handleSubmit = (e) =>{
   e.preventDefault()
+
 }
+
   render(){
+
     return(
       <>
   <form onSubmit={this.handleSubmit}>
       <label>
             Adress mail:
-            <input type="mail" id='mail' onChange={this.handleEmailChange} value={this.state.email}/>
+            <input type="mail" id='mail'  value={this.state.email} onChange={this.handleEmailChange}/>
       </label>
       <label>
           Mot de pass:
-          <input type="password" onChange={this.handlePasswordChange} value={this.state.password }/>
+          <input type="password" value={this.state.password } onChange={this.handlePasswordChange} />
       </label>
       <input type="submit" />
   </form>
+
       </>
     )
   }
